0e241a961f3eff6cc1428f98e21f3fda42695384
Andrej Pancik <andrej@pancik.com>
2012-10-05 02:37:37 +0100

    Cleanup

--- LunarLander/src/uk/ac/ox/pancik/lunarlander/actions/NoOperation.java
+++
@@ -1,9 +0,0 @@
-package uk.ac.ox.pancik.lunarlander.actions;
-
-import uk.ac.ox.pancik.sarsa.actions.Action;
-
-public class NoOperation implements Action {
-	@Override
-	public void execute() {
-	}
-}
--- LunarLander/src/uk/ac/ox/pancik/lunarlander/rewarders/Rewarder.java
+++
@@ -1,6 +0,0 @@
-package uk.ac.ox.pancik.lunarlander.rewarders;
-
-// implementation of eval(X) - utility of the actual state
-public interface Rewarder {
-	public double calculateReward();
-}
